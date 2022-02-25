3fc40935455ceda4d84058101d9f604875fda34c
Venky <venkatramanm@gmail.com>
2022-02-25 15:28:27 +0530

    Fix version

--- src/main/java/in/succinct/beckn/Email.java
+++
@@ -1,9 +0,0 @@
-package in.succinct.beckn;
-
-public class Email extends Tag{
-    public static final String TAG_NAME = "email";
-    public Email(){
-        super();
-        setKey(TAG_NAME);
-    }
-}
